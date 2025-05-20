6ba010ff21811d3bb973d55ef8e1bfb319bb3de1
Chris <Chris Davie>
2025-05-20 09:46:14 +0100

    Sliding window median, a mess

---
+++ l_0480_sliding_window_median/previous_0/solution.py
@@ -0,0 +1,60 @@
+"""
+Taken from leetcode
+
+This, I think, reflects the best one, it incoorporates two bits of knowledge.
+The first is that, each removing and adding to the list can only move the
+median one position (independently), and so you can lazy-delete.
+
+The other (that comes from the above) is that, once you have the window full,
+if you add and remove from different sides, you need to shift the numbers in
+stack to retain the balance between the cleaned heaps
+"""
+import heapq
+from typing import List
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        if k == 1:
+            return nums
+            
+        small = []  # max heap
+        large = []  # min heap - can have 1 more elements than small
+        medians = []
+        for j in range(k):
+            heapq.heappush(large, (nums[j], j))
+
+        for _ in range(k // 2):
+            num = heapq.heappop(large) # move half of the nums from large to small to keep balance
+            heapq.heappush(small, (-num[0], num[1]))
+
+        medians.append(self.get_median(small, large, k))
+
+        for i in range(k, len(nums)):
+            if nums[i] >= large[0][0]:  # true if nums[k] belongs to large heap
+                heapq.heappush(large, (nums[i], i))
+                if nums[i - k] <= -small[0][0]:  # means the outgoing number is in small heap, which will be removed soon in the while loop below
+                    self.move(large, small)
+            else: # goto this branch if nums[i] belongs to small heap
+                heapq.heappush(small, (-nums[i], i)) 
+                if nums[i - k] >= large[0][0]: # means the outgoing number is in small heap
+                    self.move(small, large)
+
+            # # only need to pop the number if the number is at the top of the heap and it is out of the window 
+            while large and large[0][1] < (i - k + 1): 
+                heapq.heappop(large)
+            while small and small[0][1] < (i - k + 1):
+                heapq.heappop(small)
+
+            medians.append(self.get_median(small, large, k))
+
+        return medians
+
+    def get_median(self, small, large, k):
+        if k % 2 == 1:
+            return large[0][0]
+        else:
+            return (-small[0][0] + large[0][0]) / 2
+
+    def move(self, fromHeap, toHeap):
+        num, idx = heapq.heappop(fromHeap)
+        heapq.heappush(toHeap, (-num, idx))
--- l_0480_sliding_window_median/solution.py
+++ l_0480_sliding_window_median/solution.py
@@ -1,60 +1,17 @@
 """
-This isn't working, I'm going to revisit the median tomorrow from a
-different direction
+Taken from leetcode
+
+This, I think, reflects the best one, it incoorporates two bits of knowledge.
+The first is that, each removing and adding to the list can only move the
+median one position (independently), and so you can lazy-delete.
+
+The other (that comes from the above) is that, once you have the window full,
+if you add and remove from different sides, you need to shift the numbers in
+stack to retain the balance between the cleaned heaps
 """
-from collections import deque
-from heapq import heappush, heappop
+import heapq
 from typing import List
 
-class Slider:
-
-    def __init__(self, window_size: int):
-        self._smaller: list[tuple[int,int]] = []
-        self._larger: list[tuple[int,int]] = []
-
-        self._count: int = 0
-        self._window_size: int = window_size
-        self._nums_window: deque[int] = deque()
-
-    def _prune(self):
-        while self._smaller and self._smaller[0][1] < self._count - self._window_size:
-            heappop(self._smaller)
-        while self._larger and self._larger[0][1] < self._count - self._window_size:
-            heappop(self._larger)
-
-    def add(self, num: int) -> None:
-
-        # sometimes remove
-        to_remove: int | None = None
-        if len(self._nums_window) > self._window_size:
-            to_remove = self._nums_window.popleft()
-
-        # always adding to the heap
-        self._nums_window.append(num)
-        if not self._smaller or num <= -self._smaller[0][0]:
-            heappush(self._smaller, (-num, self._count))
-        else:
-            heappush(self._larger, (num, self._count))
-
-        self._count += 1
-
-    def median(self) -> float:
-        if not self._window_size % 2:
-            return float((-self._smaller[0][0] + self._larger[0][0])/2)
-        return float(-self._smaller[0][0])
-
-
 class Solution:
-
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        
-        slider = Slider(k)
-        for n in nums[:k-1]:
-            slider.add(n)
-
-        results: list[float] = []
-        for n in nums[k-1:]:
-            slider.add(n)
-            results.append(slider.median())
-
-        return results
+        return float("inf")
