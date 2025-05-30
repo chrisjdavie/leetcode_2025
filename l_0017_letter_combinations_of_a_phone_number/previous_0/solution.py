8a7ce967c903a762de3f057d5385a26d552c555b
Chris <Chris Davie>
2025-05-30 11:20:56 +0100

    Letter combinations of a phone number, revisited

--- l_0017_letter_combinations_of_a_phone_number/previous_0/solution.py
+++ l_0017_letter_combinations_of_a_phone_number/previous_0/solution.py
@@ -1,6 +1,6 @@
 from typing import List
 
-DIGITS_LETTERS_MAP: dict[int,str] = {
+digits_to_letters: dict[str, str] = {
     "2": "abc",
     "3": "def",
     "4": "ghi",
@@ -8,19 +8,18 @@ DIGITS_LETTERS_MAP: dict[int,str] = {
     "6": "mno",
     "7": "pqrs",
     "8": "tuv",
-    "9": "wxyz",    
+    "9": "wxyz",
 }
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
-            return []
+            return []        
 
-        combs: List[str] = [""]
+        combs: list[str] = [""]
         for d in digits:
-            new_combs: List[str] = []
+            new_combs: list[str] = []
             for c in combs:
-                new_combs.extend(c + l for l in DIGITS_LETTERS_MAP[d])
+                new_combs.extend([c + l for l in digits_to_letters[d]])
             combs = new_combs
-
         return combs
